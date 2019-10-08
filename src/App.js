a1f2818ffc1306ca38d46583162396aeacda6217
Matt Anderson <matt.anderson.inf@gmail.com>
2019-10-07 23:29:12 -0400

    Captions and titles added to the html
    Captions and titles added to the html

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// App.js
+
+$(document).ready(function(){
+    // grab the information from of the search input
+    var selectableList = $('.gallery');
+
+    selectableList.selectable({
+        stop: getSelectedItems
+    })
+
+    fucntion getSelectedItems(){
+        var selectedItems = '';
+        $('.ui')
+    }
+})
