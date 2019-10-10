ceb5a6742327b1e76643052aa2c2ef429bf157eb
Matt Anderson <matt.anderson.inf@gmail.com>
2019-10-09 22:52:10 -0400

    Just cleaning up
    Just cleaning up, try not to use var as much as possible as per ES6 guidelines

--- js/App.js
+++ js/App.js
@@ -1,11 +1,11 @@
-// App.js
+// app.js
 
 $(document).ready(function(){
     // grab the information from of the search input
     $("#search").on("keyup", function(){
         // grab the text that the user has typed
         //use toLowerCase so that it will still return the correct result if the user uses upper case
-        var searchable = $(this).val().toLowerCase();
+        let searchable = $(this).val().toLowerCase();
         /* 
         Toggle the visibility of the figures that have the photo-container class.
         If the searchable text matches part of the text within the figure, it will be visible, you won't see it otherwise.
